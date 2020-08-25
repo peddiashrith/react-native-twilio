e4133a6f8a38c8b26e4045cf470532063d49f5b2
peddiashrith <ashrithkumarpeddi@gmail.com>
2020-08-25 18:00:01 +0530

    added welcome page

---
+++ src/components/Welcome/Welcome.css.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    offer: {
+        width: '100%',
+        color: 'white',
+        backgroundColor: '#f464a4',
+        justifyContent: 'center',
+        display: 'flex',
+        textTransform: 'uppercase',
+        padding: '2px',
+    },
+});
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+export class Welcome extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Register and enjoy free credits worth 200. Talk to expert
+                    now.
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default View;
